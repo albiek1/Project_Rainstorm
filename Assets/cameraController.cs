cd6086083742d213a0e0a2a01deed3a6705c845c
albiek1 <54135401+albiek1@users.noreply.github.com>
2022-09-27 15:25:02 +0200

    first commit

---
+++ Assets/cameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform trackedObject;
+    public float maxDistance = 10f;
+    public float moveSpeed = 20f;
+    public float updateSpeed = 10f;
+    [Range(0, 10)]
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+        gameObject.transform.position = new Vector3(transform.position.x + (xMove * speed*Time.deltaTime), 0.5f, transform.position.z + (zMove * speed*Time.deltaTime));
+    }
+}
